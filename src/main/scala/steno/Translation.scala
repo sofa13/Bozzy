172cccada534f97141a2d7819a5f19b6fa6932ac
Ted Morin <theodore.morin@clearwaterclinical.com>
2016-02-09 23:42:45 -0500

    Slightly improve robustness of translation detection in RTF

--- src/main/scala/steno/Translation.scala
+++ src/main/scala/steno/Translation.scala
@@ -10,9 +10,10 @@ class Translation(entry: String, format: DictionaryFormat.Value)  {
       case stroke(rawStroke, rawTranslation) => rawTranslation
     }
   } else if (format == DictionaryFormat.RTF) {
-    val stroke = """\{\\\*\\cxs ([^}]+)\}([^{]+).*$""".r
+    val stroke = """\{\\\*\\cxs ([^}]+)\}(.+?)(\{\\\*\\c.*)?""".r
     entry match {
       case stroke(rawStroke, rawTranslation) => rawTranslation
+      case stroke(rawStroke, rawTranslation, extra) => rawTranslation
     }
   }
 }
